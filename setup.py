8f483eb9a4b9599e7d8b7b16e8077c5dc1fcb796
Karsten Wolf <karstenwo@web.de>
2022-03-31 23:58:55 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='squeaklib',
       version='1.0',
-      py_modules=['squeaklib'],
+      packages=['squeaklib'],
+      install_requires=[
+            'photobot; python_version >= "2.7.0"',
+            'Pillow'],
       )
+
