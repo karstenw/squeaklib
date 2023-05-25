77ef68f25ec7c1656f623a4016ac9d358050fb54
Karsten Wolf <karstenwo@web.de>
2023-05-25 13:03:22 +0200

    py3

--- squeaklib/makepoint.py
+++ squeaklib/makepoint.py
@@ -1,23 +1,20 @@
-py3 = False
 try:
-    unicode('')
+    unicode
 except NameError:
     long = int
 
 def makePoint( *args  ):
     "Tries to create a Point from args."
-    from . import point
     n = len(args)
     if n == 1:
         typ = type( args[0] )
-        if typ in (point.Point,):
+        if typ in (Point,):
             return args[0]
         elif typ in (long, int, float):
-            return point.Point( args[0], args[0] )
+            return Point( args[0], args[0] )
         elif typ in (list, tuple):
-            return point.Point( args[0][0], args[0][1] )
+            return Point( args[0][0], args[0][1] )
     elif n == 2:
-        return point.Point( args[0], args[1] )
+        return Point( args[0], args[1] )
     return None
-
 
