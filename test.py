f9fcca2f20b34c4f306f98db2e695ed05948b3ff
Karsten Wolf <karstenwo@web.de>
2025-06-27 11:29:25 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import squeaklib
+p1 = squeaklib.Point(30, 120)
+p2 = squeaklib.Point(75, 135)
+print("p1:", p1)
+print("p2:", p2)
+print("p2-p1:", p2-p1 )
+print("(p2-p1) / 2:", (p2-p1) / 2)
+print("p1 + ((p2-p1) / 2):", p1 + ((p2-p1) / 2))
+
