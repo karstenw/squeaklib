f4cbdd37276137e3a6fcb05011b7c47ad3eb80e9
Karsten Wolf <karstenwo@web.de>
2025-09-21 15:54:51 +0200

    3-digit version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='squeaklib',
-      version='1.0',
+      version='1.0.0',
       packages=['squeaklib'],
       install_requires=[
             'photobot; python_version >= "2.7.0"',
